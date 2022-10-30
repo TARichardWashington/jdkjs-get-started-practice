e3a53beda990f24db18ce1d2732e9a6dd04229a0
Richard Washington <richard.washington@taxassist.co.uk>
2022-10-30 12:17:24 +0000

    :sparkles: Range is correct for positive integers in the correct order

--- closures.js
+++ closures.js
@@ -1,7 +1,16 @@
 function range(start, end) {
-    return [3];
+    if(typeof(start) === 'number' && typeof(end) === 'number') {
+        var result = [];
+        for(let i = start; i <= end; i ++) {
+            result.push(i);
+        }
+        return result;
+    }
+
 }
 
 var rangeArray = range(3, 3); // [3]
+console.log(rangeArray);
 
+var rangeArray = range(3, 8); // [3, 4, 5, 6, 7, 8]
 console.log(rangeArray);
