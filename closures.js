7dbcc7e4cd2d6fa8824228518b73a7dc3e4a891e
Richard Washington <richard.washington@taxassist.co.uk>
2022-10-30 12:27:06 +0000

    :sparkles: Return function with a closure over the outer function start

--- closures.js
+++ closures.js
@@ -1,10 +1,20 @@
 function range(start, end) {
     if(typeof(start) === 'number' && typeof(end) === 'number') {
         var result = [];
+        
         for(let i = start; i <= end; i ++) {
             result.push(i);
         }
         return result;
+    } else {
+        return function(endNum) {
+            var result = [];
+            for(let i = start; i <= endNum; i ++) {
+                result.push(i);
+            }
+    
+            return result;
+        }
     }
 
 }
@@ -16,4 +26,18 @@ var rangeArray = range(3, 8); // [3, 4, 5, 6, 7, 8]
 console.log(rangeArray);
 
 var rangeArray = range(3, 0); // []
+console.log(rangeArray);
+
+var start3 = range(3);
+var rangeArray = start3(3); // [3]
+console.log(rangeArray);
+
+var rangeArray = start3(8); // [3, 4, 5, 6, 7, 8]
+console.log(rangeArray);
+
+var rangeArray = start3(0); // []
+console.log(rangeArray);
+
+var start4 = range(4);
+var rangeArray = start4(6); // [4, 5, 6]
 console.log(rangeArray);
